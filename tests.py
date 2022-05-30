185e37e0eb8f4de5f0b2f477ffc5ae18085b59af
diegoLeo512 <77777060+diegoLeo512@users.noreply.github.com>
2022-05-30 01:49:43 -0500

    Terminado

--- tests.py
+++ tests.py
@@ -41,14 +41,7 @@ class FleskTest(unittest.TestCase):
         response = tester.post('/books')
         statcode = response.status_code
         self.assertEqual(statcode,200)
-    
-    #Checar post book sin parametros
-    def test__post_without_parameters(self):
-        tester = app.test_client(self)
-        response = tester.post('/books')
-        statcode = response.status_code
-        self.assertEqual(statcode,200)
-    
+        
     #Checar post book con parametros incorrectos
     def test__post_with_wrong_parameters(self):
         tester = app.test_client(self)
@@ -66,28 +59,21 @@ class FleskTest(unittest.TestCase):
     ######################################################################################
 
     #Checar put book sin parametros
-    def test__post_without_parameters(self):
-        tester = app.test_client(self)
-        response = tester.put('/books')
-        statcode = response.status_code
-        self.assertEqual(statcode,200)
-    
-    #Checar post book sin parametros
-    def test__post_without_parameters(self):
+    def test_put_without_parameters(self):
         tester = app.test_client(self)
         response = tester.put('/books')
         statcode = response.status_code
         self.assertEqual(statcode,200)
     
     #Checar put book con parametros incorrectos
-    def test__post_with_wrong_parameters(self):
+    def test_put_with_wrong_parameters(self):
         tester = app.test_client(self)
         response = tester.put('/books?name=prueba 8&date=28-21-2222')
         statcode = response.status_code
         self.assertEqual(statcode,200)
 
     #Checar put book con parametros correctos
-    def test__post_with_rigth_parameters(self):
+    def test_put_with_rigth_parameters(self):
         tester = app.test_client(self)
         response = tester.put('/books?name=prueba 8&date=28-04-2022')
         statcode = response.status_code
@@ -97,14 +83,14 @@ class FleskTest(unittest.TestCase):
 
 
     #checar delete book sin parametros
-    def test__get_without_parameters(self):
+    def test_delete_without_parameters(self):
         tester = app.test_client(self)
         response = tester.delete('/books')
         statcode = response.status_code
         self.assertEqual(statcode,200)
 
     #checar get book con parametros
-    def test__get_with_parameters(self):
+    def test_delete_with_parameters(self):
         tester = app.test_client(self)
         response = tester.delete('/books?name=prueba N')
         statcode = response.status_code
